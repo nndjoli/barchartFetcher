2ca45da423b021af17f8ce698720ae211ba7c31d
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-12 17:23:02 +0200

    add query_auth_class.py

---
+++ barchartFetcher/utils/query_auth_class.py
@@ -0,0 +1,21 @@
+import httpx
+import urllib.parse
+
+
+class QueryAuthClass:
+
+    def __init__(self):
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"
+        }
+        self.cookies = self.get_cookies()
+        self.XSRFToken = urllib.parse.unquote(self.cookies["XSRF-TOKEN"])
+        self.headers["x-xsrf-token"] = self.XSRFToken
+
+    def get_cookies(self):
+        response = httpx.get(
+            "https://www.barchart.com/stocks/quotes/AAPL/overview",
+            headers=self.headers,
+        )
+        cookies = response.cookies
+        return cookies
