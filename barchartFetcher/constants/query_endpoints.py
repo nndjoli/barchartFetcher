3c32877116466416f8df991a2020721cd99825a8
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-14 09:21:29 +0200

    Add constants

---
+++ barchartFetcher/constants/query_endpoints.py
@@ -0,0 +1,16 @@
+class QueryEndpoints:
+
+    def __init__(self):
+
+        self.base_url = "https://www.barchart.com"
+        self.quote = "https://www.barchart.com/proxies/core-api/v1/quotes/get?"
+        self.options = "https://www.barchart.com/proxies/core-api/v1/options/get?"
+        self.options_flow = "https://www.barchart.com/proxies/core-api/v1/options/flow?"
+        self.options_expirations = "https://www.barchart.com/proxies/core-api/v1/options-expirations/get?"
+        self.earnings_dividends = "https://www.barchart.com/proxies/core-api/v1/earnings-dividends/get?"
+        self.momentum = "https://www.barchart.com/proxies/core-api/v1/momentum/get?"
+        self.historical = "https://www.barchart.com/proxies/core-api/v1/historical/get?"
+        self.insider_trades = "https://www.barchart.com/proxies/core-api/v1/insiderTrades/get?"
+        self.sec_filings = "https://www.barchart.com/proxies/core-api/v1/sec-filings/get?"
+        self.options_historical = "https://www.barchart.com/proxies/core-api/v1/options-historical/get?"
+        self.historical_volatility = "https://www.barchart.com/proxies/core-api/v1/historical-volatility/get?"
