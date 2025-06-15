589aebde9b3ca669a6715e9afa982aa3ea413f11
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-15 18:56:40 +0200

    Create OpinionFields class

--- barchartFetcher/constants/technicals/barchart_opinion.py
+++ barchartFetcher/constants/technicals/barchart_opinion.py
@@ -1,5 +1,5 @@
 # Opinion Fields
-OPINION_OPINION_FIELDS = (
+OPINION_FIELDS = (
     "opinion,opinionSignal,opinionPercent,opinionStrength,opinionDirection,"
     "opinionChange,opinionPrevious,opinionPreviousSignal,opinionPreviousPercent,"
     "opinionLastWeek,opinionLastWeekSignal,opinionLastWeekPercent,opinionLastMonth,"
@@ -10,12 +10,12 @@ OPINION_OPINION_FIELDS = (
 )
 
 # Composite Indicator Fields
-OPINION_COMPOSITE_INDICATOR_FIELDS = (
+COMPOSITE_INDICATOR_FIELDS = (
     "trendSpotterSignal,trendSpotterStrength,trendSpotterDirection"
 )
 
 # Short Term Indicators Fields
-OPINION_SHORT_TERM_INDICATORS_FIELDS = (
+SHORT_TERM_INDICATORS_FIELDS = (
     "movingAverage20dSignal,movingAverage20dStrength,movingAverage20dDirection,"
     "macd20to50dSignal,macd20to50dStrength,macd20to50dDirection,macd20to100dSignal,"
     "macd20to100dStrength,macd20to100dDirection,macd20to200dSignal,"
@@ -23,7 +23,7 @@ OPINION_SHORT_TERM_INDICATORS_FIELDS = (
 )
 
 # Medium Term Indicators Fields
-OPINION_MEDIUM_TERM_INDICATORS_FIELDS = (
+MEDIUM_TERM_INDICATORS_FIELDS = (
     "movingAverage50dSignal,movingAverage50dStrength,movingAverage50dDirection,"
     "macd50to100dSignal,macd50to100dStrength,macd50to100dDirection,"
     "macd50to150dSignal,macd50to150dStrength,macd50to150dDirection,"
@@ -31,9 +31,19 @@ OPINION_MEDIUM_TERM_INDICATORS_FIELDS = (
 )
 
 # Long Term Indicators Fields
-OPINION_LONG_TERM_INDICATORS_FIELDS = (
+LONG_TERM_INDICATORS_FIELDS = (
     "movingAverage100dSignal,movingAverage100dStrength,movingAverage100dDirection,"
     "movingAverage150dSignal,movingAverage150dStrength,movingAverage150dDirection,"
     "movingAverage200dSignal,movingAverage200dStrength,movingAverage200dDirection,"
     "macd100to200dSignal,macd100to200dStrength,macd100to200dDirection"
 )
+
+
+class OpinionFields:
+
+    def __init__(self):
+        self.fields = OPINION_FIELDS
+        self.composite_indicator_fields = COMPOSITE_INDICATOR_FIELDS
+        self.short_term_indicators_fields = SHORT_TERM_INDICATORS_FIELDS
+        self.medium_term_indicators_fields = MEDIUM_TERM_INDICATORS_FIELDS
+        self.long_term_indicators_fields = LONG_TERM_INDICATORS_FIELDS
