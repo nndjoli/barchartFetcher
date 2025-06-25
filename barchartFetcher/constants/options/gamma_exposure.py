12f5b7e8ea755fe6abc5e0bd89092f7ad207ccee
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-25 09:53:30 +0200

    Update gamma_exposure.py

---
+++ barchartFetcher/constants/options/gamma_exposure.py
@@ -0,0 +1,21 @@
+# GEX Fields:
+GAMMA_EXPOSURE_FIELDS = (
+    "fields=symbol,strikePrice,"
+    "optionType,baseDailyLastPrice,baseLastPrice,dailyGamma,"
+    "gamma,delta,dailyDelta,dailyOpenInterest,openInterest,dailyVolume,"
+    "volume,daysToExpiration,expirationDate,averageVolatility,"
+    "&expirations=OPTIONS_EXPIRATIONS_FIELDS&le(nearestToLast,100)="
+)
+
+
+class GEXFields:
+    def __init__(self):
+
+        self.gamma_exposure_fields = GAMMA_EXPOSURE_FIELDS
+        self.gamma_exposure_endpoint = "options"
+        self.gamma_exposure_requires = print(
+            "Need to replace OPTIONS_EXPIRATIONS_FIELDS by the queried symbol options expiraitons.\n"
+            "options_expirations_url = options_expirations_endpt + f'symbols=symbol&fields={options_expirations_fields}&raw=1\n"
+            "options_expirations = QueryManager().sync_query(options_expirations_url)\n"
+            "(','.join(options_expirations))"
+        )
