dab4dc59c330e3324c462ba9471702c9ea24072f
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-26 09:25:08 +0200

    Update build_query_string_dictionary function input parameters

--- barchartFetcher/utils/query_strings_manager.py
+++ barchartFetcher/utils/query_strings_manager.py
@@ -1,5 +1,4 @@
-def build_query_string_dictionary(QueryManager, complete_dataframe):
-
+def build_query_string_dictionary(complete_dataframe):
     query_strings_dict = {}
 
     fields_dataframe = complete_dataframe
@@ -10,9 +9,7 @@ def build_query_string_dictionary(QueryManager, complete_dataframe):
         if category not in query_strings_dict:
             query_strings_dict[category] = {}
 
-        category_fields_dataframe = fields_dataframe[
-            fields_dataframe["category"] == category
-        ]
+        category_fields_dataframe = fields_dataframe[fields_dataframe["category"] == category]
         subcategories = category_fields_dataframe["subcategory"].unique()
 
         for subcategory in subcategories:
@@ -21,17 +18,11 @@ def build_query_string_dictionary(QueryManager, complete_dataframe):
 
                 query_strings_dict[category][subcategory] = ""
 
-            subcategory_fields_dataframe = category_fields_dataframe[
-                category_fields_dataframe["subcategory"] == subcategory
-            ]
-            subcategory_fields_list = subcategory_fields_dataframe[
-                "apiField"
-            ].tolist()
+            subcategory_fields_dataframe = category_fields_dataframe[category_fields_dataframe["subcategory"] == subcategory]
+            subcategory_fields_list = subcategory_fields_dataframe["apiField"].tolist()
             subcategory_fields_string = ",".join(subcategory_fields_list)
 
-            query_strings_dict[category][
-                subcategory
-            ] = subcategory_fields_string
+            query_strings_dict[category][subcategory] = subcategory_fields_string
 
     for category, subcategories in query_strings_dict.items():
         if len(subcategories.keys()) == 1:
@@ -40,10 +31,8 @@ def build_query_string_dictionary(QueryManager, complete_dataframe):
 
 
 class QueryStringsManager:
-    def __init__(self, QueryManager, complete_dataframe):
+    def __init__(self, complete_dataframe):
         self.complete_dataframe = complete_dataframe
         """Complete DataFrame with all fields."""
-        self.query_strings_dict = build_query_string_dictionary(
-            QueryManager, complete_dataframe
-        )
+        self.query_strings_dict = build_query_string_dictionary(complete_dataframe)
         """Dictionary with query strings for each category and subcategory."""
