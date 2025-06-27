f279bb0bcbf1d68df2cf509613fc1b64ed9b1d72
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 14:55:55 +0200

    __init.py

---
+++ barchartFetcher/constants/company/__init__.py
@@ -0,0 +1,4 @@
+from .competitors import SectorCompetitorsFields
+from .insider_trades import InsiderTradesFields
+from .key_statistics import KeyStatisticsFields
+from .sec_filings import SECFilingsFields
