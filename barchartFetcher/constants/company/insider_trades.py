1360adce03e6c9c23fd33a86f5817f4f319df1be
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 14:57:02 +0200

    Add insider_trades.py

---
+++ barchartFetcher/constants/company/insider_trades.py
@@ -0,0 +1,21 @@
+# Insider trades fields
+INSIDER_TRADES_FIELDS = (
+    "fullName,shortJobTitle,transactionType,transactionDate,"
+    "amount,reportedPrice,usdValue,eodHolding,eodHoldingPercentage,symbolCode,"
+    "hasOptions,symbolType,lastPrice,dailyLastPrice"
+)
+
+
+class InsiderTradesFields:
+    def __init__(self):
+        self.insider_trades_fields = {
+            "endpoint": "insiderTrades/get",
+            "symbol_param": "eq(symbol|<symbol>)",
+            "eq(symbol|<symbol>)": "",
+            "orderBy": "transactionDate",
+            "orderDir": "desc",
+            "meta": "field.shortName,field.type,field.description",
+            "page": 1,
+            "limit": 100,
+            "raw": 1,
+        }
