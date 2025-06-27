757fc5d6d1e4ee26630c12c41edda5bd44cf44a7
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:01:35 +0200

    Update options_flow.py

--- barchartFetcher/constants/options/options_flow.py
+++ barchartFetcher/constants/options/options_flow.py
@@ -1,11 +1,9 @@
 # Options Flow Fields
 OPTIONS_FLOW_FIELDS = (
-    "symbol,baseSymbol,lastPrice,symbolType,strikePrice,"
-    "expiration,dte,bidXSize,askXSize,tradePrice,tradeSize,"
-    "side,premium,volume,openInterest,volatility,delta,"
-    "tradeCondition,label,tradeTime.format(H:i:s \\E\\T),"
-    "expirationType,askPrice,bidPrice,baseSymbolType,"
-    "symbolCode"
+    "symbol,baseSymbol,lastPrice,symbolType,strikePrice,expiration,dte,bidXSize,"
+    "askXSize,tradePrice,tradeSize,side,premium,volume,openInterest,volatility,"
+    "delta,tradeCondition,label,tradeTime.format(H:i:s \\E\\T),expirationType,"
+    "askPrice,bidPrice,baseSymbolType,symbolCode"
 )
 
 # Bullish/ Bearish Sentiment Fields
@@ -15,10 +13,24 @@ BULLISH_BEARISH_SENTIMENT_FIELDS = (
 
 
 class OptionsFlowFields:
-
     def __init__(self):
+        self.options_flow_fields = {
+            "endpoint": "options-flow",
+            "symbol_param": "symbol",
+            "fields": OPTIONS_FLOW_FIELDS,
+            "orderBy": "premium",
+            "orderDir": "desc",
+            "limit": 3,
+            "gt(tradeSize,10)": "",
+            "meta": "field.shortName,field.description,field.type",
+            "raw": 1,
+        }
 
-        self.options_flow_fields = OPTIONS_FLOW_FIELDS
-        self.bullish_bearish_sentiment_fields = (
-            BULLISH_BEARISH_SENTIMENT_FIELDS
-        )
+        self.bullish_bearish_sentiment_fields = {
+            "endpoint": "options-flow",
+            "symbol_param": "symbol",
+            "fields": BULLISH_BEARISH_SENTIMENT_FIELDS,
+            "in(sentiment,(Bearish,Bullish))": "",
+            "raw": 1,
+            "meta": "field.shortName,field.description,field.type",
+        }
