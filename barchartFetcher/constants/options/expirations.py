ffeb5a699d39229e7be001d834097f4cff48df41
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:00:39 +0200

    Update expirations.py

--- barchartFetcher/constants/options/expirations.py
+++ barchartFetcher/constants/options/expirations.py
@@ -1,12 +1,16 @@
 # Options Expirations Fields
 EXPIRATIONS_FIELDS = (
-    "callVolume,putVolume,putCallVolumeRatio,callOpenInterest,"
-    "putOpenInterest,putCallOpenInterestRatio,expirationDate,"
-    "expirationType"
+    "callVolume,putVolume,putCallVolumeRatio,callOpenInterest,putOpenInterest,"
+    "putCallOpenInterestRatio,expirationDate,expirationType"
 )
 
 
 class ExpirationsFields:
-
     def __init__(self):
-        self.expirations_fields = EXPIRATIONS_FIELDS
+        self.expirations_fields = {
+            "endpoint": "options-expirations",
+            "symbol_param": "symbols",
+            "fields": EXPIRATIONS_FIELDS,
+            "meta": "field.shortName,field.description,field.type",
+            "raw": 1,
+        }
