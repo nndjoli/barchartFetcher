798eee06a2bec6497a3f136e8ac518d7f314d309
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:00:59 +0200

    Update gamma_exposure.py

--- barchartFetcher/constants/options/gamma_exposure.py
+++ barchartFetcher/constants/options/gamma_exposure.py
@@ -1,22 +1,18 @@
 # GEX Fields:
 GAMMA_EXPOSURE_FIELDS = (
-    "fields=symbol,strikePrice,"
-    "optionType,baseDailyLastPrice,baseLastPrice,dailyGamma,"
-    "gamma,delta,dailyDelta,dailyOpenInterest,openInterest,dailyVolume,"
-    "volume,daysToExpiration,expirationDate,averageVolatility,"
-    "&expirations=OPTIONS_EXPIRATIONS_FIELDS&le(nearestToLast,100)="
+    "symbol,strikePrice,optionType,baseDailyLastPrice,baseLastPrice,dailyGamma,"
+    "gamma,delta,dailyDelta,dailyOpenInterest,openInterest,dailyVolume,volume,"
+    "daysToExpiration,expirationDate,averageVolatility"
 )
 
 
 class GEXFields:
     def __init__(self):
-
-        self.gamma_exposure_fields = GAMMA_EXPOSURE_FIELDS
-        self.gamma_exposure_endpoint = "options"
-        self.gamma_exposure_requires = print(
-            "Need to replace OPTIONS_EXPIRATIONS_FIELDS by the queried symbol options expiraitons.\n"
-            "options_expirations_url = options_expirations_endpt + f'symbols=symbol&fields={options_expirations_fields}&raw=1\n"
-            "options_expirations = QueryManager().sync_query(options_expirations_url)\n"
-            "...turn options_expirations into a list of dates."
-            "(','.join(options_expirations))"
-        )
+        self.gamma_exposure_fields = {
+            "endpoint": "options",
+            "symbol_param": "symbols",
+            "fields": GAMMA_EXPOSURE_FIELDS,
+            "expirations": "Example: 2025-06-27,2025-07-03",
+            "groupBy": "strikePrice",
+            "le(nearestToLast,100)": "",
+        }
