4b326b5108f5be229838f699203c8a6a549e1c16
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:00:21 +0200

    Update expected_move.py

--- barchartFetcher/constants/options/expected_move.py
+++ barchartFetcher/constants/options/expected_move.py
@@ -1,10 +1,8 @@
 # Expected Move Fields
 EXPECTED_MOVE_FIELDS = (
-    "expirationDate,expirationType,daysToExpiration,"
-    "baseLastPrice,impliedMove,impliedMovePercent,"
-    "baseUpperPrice,baseLowerPrice,averageVolatility,"
-    "baseNextEarningsDate,baseTimeCode,symbolCode,"
-    "symbolType"
+    "expirationDate,expirationType,daysToExpiration,baseLastPrice,impliedMove,"
+    "impliedMovePercent,baseUpperPrice,baseLowerPrice,averageVolatility,"
+    "baseNextEarningsDate,baseTimeCode,symbolCode,symbolType"
 )
 
 # Earnings Fields
@@ -13,5 +11,21 @@ EARNINGS_FIELDS = "tradeTime.format(Y-m-d),value,event"
 
 class ExpectedMoveFields:
     def __init__(self):
-        self.expected_move_fields = EXPECTED_MOVE_FIELDS
-        self.earnings_fields = EARNINGS_FIELDS
+        self.expected_move_fields = {
+            "endpoint": "options-expirations",
+            "symbol_param": "symbol",
+            "fields": EXPECTED_MOVE_FIELDS,
+            "orderBy": "expirationDate",
+            "orderDir": "asc",
+            "meta": "field.shortName,field.description,field.type",
+            "raw": 1,
+            "page": 1,
+            "limit": 100,
+        }
+        self.earnings_fields = {
+            "endpoint": "historical",
+            "symbol_param": "symbol",
+            "fields": EARNINGS_FIELDS,
+            "type": "events",
+            "events": "earnings",
+        }
