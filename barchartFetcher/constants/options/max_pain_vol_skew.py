1620585e368e3ed1937ff280d2d62e7c43dfb527
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:01:16 +0200

    Update max_pain_vol_skew.py

--- barchartFetcher/constants/options/max_pain_vol_skew.py
+++ barchartFetcher/constants/options/max_pain_vol_skew.py
@@ -1,19 +1,18 @@
 # Max Pain/ Vol Skew Fields:
 MAX_PAIN_VOL_SKEW_FIELDS = (
-    "symbol,strikePrice,optionType,baseLastPrice,openInterest,volume,"
-    "volatility,daysToExpiration,expirationDate,tradeTime.format(m/d/y)"
-    "&expirations=OPTIONS_EXPIRATIONS_FIELDS&le(nearestToLast,40)="
+    "symbol,strikePrice,optionType,baseLastPrice,openInterest,volume,volatility,"
+    "daysToExpiration,expirationDate,tradeTime.format(m/d/y)"
 )
 
 
 class MaxPainVolSkewFields:
     def __init__(self):
-        self.max_pain_vol_skew_fields = MAX_PAIN_VOL_SKEW_FIELDS
-        self.max_pain_vol_skew_endpoint = "options"
-        self.max_pain_vol_skew_requires = print(
-            "Need to replace OPTIONS_EXPIRATIONS_FIELDS by the queried symbol options expirations dates.\n"
-            "options_expirations_url = options_expirations_endpt + f'symbols=symbol&fields={options_expirations_fields}&raw=1\n"
-            "options_expirations = QueryManager().sync_query(options_expirations_url)\n"
-            "...turn options_expirations into a list of dates."
-            "(','.join(options_expirations))"
-        )
+        self.max_pain_vol_skew_fields = {
+            "endpoint": "options",
+            "symbol_param": "symbols",
+            "raw": 1,
+            "fields": MAX_PAIN_VOL_SKEW_FIELDS,
+            "expirations": "Example: 2025-06-27,2025-07-03,2025-07-11",
+            "groupBy": "expirationDate",
+            "le(nearestToLast,40)": "",
+        }
