ad0066e9e84253f14a96209f1f17ad576c02bba0
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-27 15:01:52 +0200

    Update options_prices.py

--- barchartFetcher/constants/options/options_prices.py
+++ barchartFetcher/constants/options/options_prices.py
@@ -1,15 +1,27 @@
 # Options Prices Fields
 OPTIONS_PRICES_FIELDS = (
-    "symbol,baseSymbol,strikePrice,expirationDate,moneyness,bidPrice,midpoint,askPrice,"
-    "lastPrice,priceChange,percentChange,volume,openInterest,openInterestChange,delta,"
-    "volatility,optionType,daysToExpiration,tradeTime,averageVolatility,historicVolatility30d,"
-    "baseNextEarningsDate,dividendExDate,baseTimeCode,impliedVolatilityRank1y,symbolCode,"
-    "symbolType,theoretical,gamma,theta,vega,rho,volumeOpenInterestRatio,itmProbability"
-    "expirationType,expiration,dte,bidXSize,askXSize,tradePrice,tradeSize,side,premium,"
-    "tradeCondition,label,baseSymbolType"
+    "symbol,baseSymbol,strikePrice,expirationDate,moneyness,bidPrice,midpoint,"
+    "askPrice,lastPrice,priceChange,percentChange,volume,openInterest,"
+    "openInterestChange,delta,volatility,optionType,daysToExpiration,tradeTime,"
+    "averageVolatility,historicVolatility30d,baseNextEarningsDate,dividendExDate,"
+    "baseTimeCode,impliedVolatilityRank1y,symbolCode,symbolType,theoretical,gamma,"
+    "theta,vega,rho,volumeOpenInterestRatio,itmProbabilityexpirationType,expiration,"
+    "dte,bidXSize,askXSize,tradePrice,tradeSize,side,premium,tradeCondition,label,"
+    "baseSymbolType"
 )
 
 
 class OptionsPricesFields:
     def __init__(self):
-        self.options_prices_fields = OPTIONS_PRICES_FIELDS
+        self.options_prices_fields = {
+            "endpoint": "options",
+            "symbol_param": "baseSymbol",
+            "fields": OPTIONS_PRICES_FIELDS,
+            "groupBy": "optionType",
+            "expirationDate": "nearest",
+            "meta": "field.shortName,field.description,field.type",
+            "orderBy": "strikePrice",
+            "orderDir": "asc",
+            "optionsOverview": "true",
+            "raw": 1,
+        }
