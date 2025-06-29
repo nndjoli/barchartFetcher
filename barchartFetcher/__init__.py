61529ae670dbc09e470bc339519f5672145dd939
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-29 15:58:40 +0200

    Update __init__.py

---
+++ barchartFetcher/__init__.py
@@ -0,0 +1,17 @@
+from .utils.sync_query_classes import (
+    Analysts,
+    Company,
+    Financials,
+    Options,
+    OptionsStrategies,
+    Technicals,
+)
+
+all = [
+    "Analysts",
+    "Company",
+    "Financials",
+    "Options",
+    "OptionsStrategies",
+    "Technicals",
+]
