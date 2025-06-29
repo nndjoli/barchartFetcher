d799a0dd78c460582f7a48ffd50863f89b882c21
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-29 15:58:12 +0200

    Update __init__.py

--- barchartFetcher/utils/__init__.py
+++ barchartFetcher/utils/__init__.py
@@ -1,5 +1,12 @@
-from . import url_builders
+import barchartFetcher.utils.sync_query_functions as SyncQueryFunctions
+
+from . import url_builders as URLBuilders
 from .query_manager import QueryManager
 from .query_tasks_dicts import QueryTasksDicts
 
-__all__ = ["QueryManager", "QueryTasksDicts", "url_builders"]
+__all__ = [
+    "QueryManager",
+    "QueryTasksDicts",
+    "URLBuilders",
+    "SyncQueryFunctions",
+]
